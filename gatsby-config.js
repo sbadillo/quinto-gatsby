41a89befa766868e21a9244d6bc719dec67d8f78
serch <sbadillo@gmail.com>
2020-01-08 08:47:17 +0100

    installed the contentful source plugin and changed config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `your_space_id`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
